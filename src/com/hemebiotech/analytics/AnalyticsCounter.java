230509d771f73c79a5deedcd936159211328e11a
Yves Lesaque <sacmusicien6735xbox@gmail.com>
2020-11-16 13:56:52 +0100

    Same Commit than previous

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,15 +3,21 @@ package com.hemebiotech.analytics;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @see ISymptomReader
+ * @see ISymptomCounter
+ * @see ISymptomWriter
+ */
 public class AnalyticsCounter {
 
     static List<String> result = new ArrayList<String>();
     static List<String> symptomsList = new ArrayList<String>();
     static List<Integer> symptomsCount = new ArrayList<Integer>();
 
+    // We declare the path to the file to read, and to the file to write
     public final static String readerFilepath = "symptoms.txt";
     public final static String writerFilepath = "result.out";
-	
+    
 	public static void main(String args[]) throws Exception {
 		ReadSymptomDataFromFile readerClass = new ReadSymptomDataFromFile(readerFilepath);
         result = readerClass.GetSymptoms();
